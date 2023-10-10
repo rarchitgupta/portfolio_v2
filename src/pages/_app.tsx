fab8ba4579fd8719be5494a8e1bb13bddd85556b
Archit Gupta <rarchitgupta@gmail.com>
2023-10-10 14:44:40 +0530

    Vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,10 +7,12 @@ import { CacheProvider } from "@chakra-ui/next-js";
 import theme from "../theme";
 import { AppProps } from "next/app";
 import Head from "next/head";
+import { Analytics } from "@vercel/analytics/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Analytics />
       <Head>
         <title>Archit Gupta</title>
       </Head>
