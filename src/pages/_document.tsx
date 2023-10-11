9a8b01eaa4f56aa9e2c043125043434443c6f150
Archit Gupta <rarchitgupta@gmail.com>
2023-10-11 10:30:04 +0530

    Emotion server cache to attempt FOUC resolution

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,7 +1,32 @@
-import NextDocument, { Html, Head, Main, NextScript } from "next/document";
+import emotionCache from "../lib/emotion-cache";
+import createEmotionServer from "@emotion/server/create-instance";
+const { extractCritical } = createEmotionServer(emotionCache);
+import NextDocument, {
+  DocumentContext,
+  Html,
+  Head,
+  Main,
+  NextScript,
+} from "next/document";
 import { ColorModeScript } from "@chakra-ui/react";
 
 export default class Document extends NextDocument {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    const styles = extractCritical(initialProps.html);
+    return {
+      ...initialProps,
+      styles: [
+        initialProps.styles,
+        <style
+          key="emotion-css"
+          dangerouslySetInnerHTML={{ __html: styles.css }}
+          data-emotion-css={styles.ids.join(" ")}
+        />,
+      ],
+    };
+  }
+
   render() {
     return (
       <Html>
