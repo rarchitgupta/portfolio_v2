2a1fdee45fa2e80bd9ebb2b3530d4ab3f36c4c68
Archit Gupta <rarchitgupta@gmail.com>
2023-10-11 16:20:06 +0530

    Favicon configuration and Meta description

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,10 +11,32 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Analytics />
       <Head>
         <title>Archit Gupta</title>
+        <meta name="description">
+          Software Engineer and Web Developer portfolio site. I love to build
+          systems and experiences and I'm available to work
+        </meta>
         <script
           data-project-id="1rzRv1KhH8z3jiGcw2UF4eAtoqYJWIxniREB7340"
           src="https://snippet.meticulous.ai/v1/meticulous.js"
         />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
       <ChakraProvider theme={theme}>
         <Component {...pageProps} />
